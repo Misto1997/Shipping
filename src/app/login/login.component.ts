0b4e8e185f9d6013246e74f6b66054fcf3efec31
Rana <768978@cognizant.com>
2019-04-23 10:51:23 +0530

    23-04-2019

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,43 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup,Validators} from '@angular/forms';
+import {FormControl,FormGroup,Validators, AbstractControl} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent {
 
   login;
-  ngOnInit()
-  {
-    this.login=new FormGroup({
-      cId:new FormControl(""),
-      pass:new FormControl(""),
-      loginType:new FormControl("")
-    });
-  }
-  constructor() { }
+  signUp;
+
+  submitted : boolean = false ;
+
   disp:boolean=true;
   disp1:boolean=false;
   temp:boolean;
 
+  ngOnInit()
+  {
+    this.login=new FormGroup({
+      loginType:new FormControl('',[Validators.required]),
+      cId:new FormControl('',[Validators.required]),
+      pass:new FormControl( '',[Validators.required])
+      
+    });
+
+    this.signUp=new FormGroup({
+      fname:new FormControl(""),
+      lname:new FormControl(""),
+      cId:new FormControl(""),
+      pass:new FormControl(""),
+      cpass:new FormControl("")
+    });
+  }
+
+  get f()
+  {
+       return this.login.controls; 
+  }
+
+  
+
+  constructor() { }
+
   changeValue(val)
   {
-    if(val=false)
+    if(val==false)
       {
         this.disp1=true;
         this.disp=false;
       }
     else
-    {
+      {
         this.disp1=false;
         this.disp=true;
-    }
+      }
 
   }
-  onSubmit(myForm)
+
+  onSubmit(login)
   {
-    console.log(myForm);
+      this.submitted = true;
+      if(this.login.invalid)   
+        return;  
+
+      console.log(login);
   }
+
+   
 
 }
