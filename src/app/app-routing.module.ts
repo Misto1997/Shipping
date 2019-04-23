ae1473de8107159fd6932d3a54982028c73eaf0c
Rawat <768998@cognizant.com>
2019-04-23 16:16:05 +0530

    23-4-2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { AdminhomeComponent } from './adminhome/adminhome.component';
 import { UserhomeComponent } from './userhome/userhome.component';
-import { UsercontactComponent } from './usercontact/usercontact.component';
+import { UsercontactComponent } from './userhome/usercontact/usercontact.component';
 import { AddEmployeeComponent } from './adminhome/add-employee/add-employee.component'
 import { DeleteUserComponent } from './adminhome/delete-user/delete-user.component';
 import { LogoutComponent } from './adminhome/logout/logout.component';
@@ -11,16 +11,21 @@ import { OrdersComponent } from './adminhome/orders/orders.component';
 import { ProfileComponent } from './adminhome/profile/profile.component';
 
 
-
 const routes: Routes =
  [
-    { path : ""  , component : LoginComponent},
-    { path : "homepage" , component : AdminhomeComponent ,                   //, canActivate : [AuthenticateGuard] ,
+    { path : "login"  , component : LoginComponent},
+    { path : "adminHome" , component : AdminhomeComponent ,                   //, canActivate : [AuthenticateGuard] ,
       children : [
                     { path : 'orders' ,  component : OrdersComponent} , 
                     { path : 'logout' ,  component : LogoutComponent} , 
                     { path : 'delUser' , component : DeleteUserComponent},
                     { path : 'profile' ,  component : ProfileComponent },
+                 ]
+    },
+    { path : "userHome" , component : UserhomeComponent ,                   //, canActivate : [AuthenticateGuard] ,
+      children : [
+                    { path : 'usercontact' ,  component :  UsercontactComponent } , 
+                    { path : 'logout' ,  component : LogoutComponent} , 
                  ]
     }
  ];
