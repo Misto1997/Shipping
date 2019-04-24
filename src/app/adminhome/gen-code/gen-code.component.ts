ec67efeff25e16cbf22c7c50b40f18607f31fdff
Rawat <768998@cognizant.com>
2019-04-24 13:56:58 +0530

    24-4-2019

---
+++ src/app/adminhome/gen-code/gen-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-code',
+  templateUrl: './gen-code.component.html',
+  styleUrls: ['./gen-code.component.css']
+})
+export class GenCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
