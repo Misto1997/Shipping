e547dfcf8b9696cc3e436c047d65fa17a678c9b2
Singh <768972@cognizant.com>
2019-04-24 12:31:28 +0530

    24-4-2019

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule,ReactiveFormsModule} from  '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserhomeComponent } from './userhome/userhome.component';
+import { FooterComponent } from './footer/footer.component';
+import { UsercontactComponent } from './userhome/usercontact/usercontact.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { OrdersComponent } from './adminhome/orders/orders.component';
+import { ProfileComponent } from './adminhome/profile/profile.component';
+import { AddEmployeeComponent } from './adminhome/add-employee/add-employee.component';
+import { DeleteUserComponent } from './adminhome/delete-user/delete-user.component';
+import { LogoutComponent } from './adminhome/logout/logout.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserhomeComponent,
+    FooterComponent,
+    UsercontactComponent,
+    AdminhomeComponent,
+    OrdersComponent,
+    ProfileComponent,
+    AddEmployeeComponent,
+    DeleteUserComponent,
+    LogoutComponent,
+   
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
