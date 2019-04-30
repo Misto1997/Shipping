117d5a24c315ede984cb5a2f8fff403eb6eaf5f9
Rawat <768998@cognizant.com>
2019-04-30 12:38:17 +0530

    30-4-2019

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -27,14 +27,15 @@ export class LoginService {
   getUser(user : LoginUser) : Observable<any>
   {
     
-    return this.http.get(this.url+"login/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response)
+    //return this.http.get(this.url+"login/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response)
+    return this.http.post(this.url+"login/" , JSON.stringify(user) , { headers : new Headers(this.headerDict) }).map((response:any)=>response)
   }
 
    postUser(user : User ) : Observable<any>
   {
 
     console.log(JSON.stringify(user));
-    return this.http.post(  this.url+"userSignUp/user", JSON.stringify(user) , { headers : new Headers(this.headerDict) , } ).map((response:any)=>response);
+    return this.http.post(  this.url+"signUp/user/", JSON.stringify(user) , { headers : new Headers(this.headerDict) , } ).map((response:any)=>response);
     
   }
 
