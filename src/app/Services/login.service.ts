6ab648fe398fa0c5f645e1a03b3b79ed536cc6fb
Rawat <768998@cognizant.com>
2019-04-30 11:58:38 +0530

    30-4-2019

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Headers ,  Http} from '@angular/http';
 import {Response} from '@angular/http/src/static_response';
 import {Observable} from 'rxjs';
 import 'rxjs/rx';
 import {LoginUser, User} from '../Classes/user';
+
 
 
 @Injectable({
@@ -13,20 +14,35 @@ export class LoginService {
 
   url:string="http://localhost:5000/";
 
-  constructor(private ob:Http) { }
+
+   headerDict = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  }
+  
+   requestOptions = {                                                                                                                                                                                 
+    headers: new Headers(this.headerDict), 
+  };
+
+  constructor(private http:Http) { }
 
   getUser(user : LoginUser) : Observable<any>
   {
     
-    return this.ob.get(this.url+"login/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response)
+    return this.http.get(this.url+"login/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response)
   }
 
    postUser(user : User ) : Observable<any>
   {
     
-    //return this.ob.post(this.url+"userSignUp/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response);
-    console.log(user);
-    return this.ob.post(this.url+"userSignUp/",user).map((response:any)=>response);
+    //return this.ob.get(this.url+"userSignUp/"+ user.type+"/"+user.mobileNo+"/"+user.pass).map((response:any)=>response);
+
+   // var jsonObj  = { "name":user.name , "emailId":user.emailId , "mobileNo":user.mobileNo , "age":user.age , "address":user.address , "pass":user.pass };
+    
+    //console.log(jsonObj);
+    console.log(JSON.stringify(user));
+    return this.http.post(  this.url+"userSignUp/", JSON.stringify(user) , this.requestOptions ).map((response:any)=>response);
     
   }
 
